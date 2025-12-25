53aeae3a7a1d88b818ea9b7312a9509d3f114602
HamzaIT2 <hmzhkymr4@gmail.com>
2025-12-25 16:00:57 +0300

    Add variable student

---
+++ lib/classes.dart
@@ -0,0 +1,19 @@
+class Student {
+  final String fullName;
+  final String studentId;
+  final String department;
+  final String stage;
+  final String email;
+  final String phone;
+  final bool isMale;
+
+  Student({
+    required this.fullName,
+    required this.studentId,
+    required this.department,
+    required this.stage,
+    required this.email,
+    required this.phone,
+    required this.isMale,
+  });
+}
