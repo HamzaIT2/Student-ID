aa9d91acce149a46b3145cb0ad457c063d9f303d
HamzaIT2 <hmzhkymr4@gmail.com>
2025-12-25 18:03:03 +0300

    Add Form and ListView to Create TextFieldForm

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -30,96 +30,95 @@ class _StudentFormScreen1State extends State<StudentFormScreen1> {
         centerTitle: true,
         backgroundColor: Colors.lime,
       ),
-      // body:
-      // Padding(
-      // padding: EdgeInsetsGeometry.all(16),
-      // child: Form(
-      //   key: _formKey,
-      //   child: ListView(
-      //     children: [
-      //       _TextFieldForm(
-      //         controller: fullNameController,
-      //         lable: 'Full Name',
-      //         hint: 'Enter Your Full Name',
-      //       ),
-      //       _TextFieldForm(
-      //         controller: stuIdController,
-      //         lable: 'Student ID',
-      //         hint: 'Enter Student Id Number',
-      //         keyboardType: TextInputType.number,
-      //       ),
-      //       _TextFieldForm(
-      //         controller: departmentController,
-      //         lable: 'Department',
-      //         hint: 'Enter Your Department',
-      //         keyboardType: TextInputType.text,
-      //       ),
-      //       _TextFieldForm(
-      //         controller: stageController,
-      //         lable: 'Stage',
-      //         hint: 'Enter The Stage',
-      //         keyboardType: TextInputType.number,
-      //       ),
-      //       _TextFieldForm(
-      //         controller: emailController,
-      //         lable: 'Email',
-      //         hint: 'Enter Your Email',
-      //         keyboardType: TextInputType.emailAddress,
-      //       ),
-      //       _TextFieldForm(
-      //         controller: phoneController,
-      //         lable: 'Phone Number',
-      //         hint: 'Enter Your Phone Number',
-      //         keyboardType: TextInputType.number,
-      //       ),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(16),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              _TextFieldForm(
+                controller: fullNameController,
+                lable: 'Full Name',
+                hint: 'Enter Your Full Name',
+              ),
+              _TextFieldForm(
+                controller: stuIdController,
+                lable: 'Student ID',
+                hint: 'Enter Student Id Number',
+                keyboardType: TextInputType.number,
+              ),
+              _TextFieldForm(
+                controller: departmentController,
+                lable: 'Department',
+                hint: 'Enter Your Department',
+                keyboardType: TextInputType.text,
+              ),
+              _TextFieldForm(
+                controller: stageController,
+                lable: 'Stage',
+                hint: 'Enter The Stage',
+                keyboardType: TextInputType.number,
+              ),
+              _TextFieldForm(
+                controller: emailController,
+                lable: 'Email',
+                hint: 'Enter Your Email',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              _TextFieldForm(
+                controller: phoneController,
+                lable: 'Phone Number',
+                hint: 'Enter Your Phone Number',
+                keyboardType: TextInputType.number,
+              ),
 
-      // SizedBox(height: 16),
-      // Divider(height: 10, thickness: 5, color: Colors.lime),
-      // SizedBox(height: 10),
-      //               Text(
-      //                 'Gender',
-      //                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-      //               ),
-      //               Column(
-      //                 children: [
-      //                   Row(
-      //                     children: [
-      //                       Radio<bool>(
-      //                         value: true,
-      //                         groupValue: Gender,
-      //                         onChanged: (value) => {
-      //                           setState(() {
-      //                             Gender = value!;
-      //                           }),
-      //                         },
-      //                       ),
-      //                       Text(
-      //                         'Male',
-      //                         style: TextStyle(fontWeight: FontWeight.bold),
-      //                       ),
-      //                       Radio<bool>(
-      //                         value: false,
-      //                         groupValue: Gender,
-      //                         onChanged: (value) => {
-      //                           setState(() {
-      //                             Gender = value!;
-      //                           }),
-      //                         },
-      //                       ),
-      //                       Text(
-      //                         'Female',
-      //                         style: TextStyle(fontWeight: FontWeight.bold),
-      //                       ),
-      //                     ],
-      //                   ),
-      //                 ],
-      //               ),
-      //               ElevatedButton(onPressed: () {}, child: Text('Submit')),
-      //             ],
+              // SizedBox(height: 16),
+              // Divider(height: 10, thickness: 5, color: Colors.lime),
+              // SizedBox(height: 10),
+              //               Text(
+              //                 'Gender',
+              //                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              //               ),
+              //               Column(
+              //                 children: [
+              //                   Row(
+              //                     children: [
+              //                       Radio<bool>(
+              //                         value: true,
+              //                         groupValue: Gender,
+              //                         onChanged: (value) => {
+              //                           setState(() {
+              //                             Gender = value!;
+              //                           }),
+              //                         },
+              //                       ),
+              //                       Text(
+              //                         'Male',
+              //                         style: TextStyle(fontWeight: FontWeight.bold),
+              //                       ),
+              //                       Radio<bool>(
+              //                         value: false,
+              //                         groupValue: Gender,
+              //                         onChanged: (value) => {
+              //                           setState(() {
+              //                             Gender = value!;
+              //                           }),
+              //                         },
+              //                       ),
+              //                       Text(
+              //                         'Female',
+              //                         style: TextStyle(fontWeight: FontWeight.bold),
+              //                       ),
+              //                     ],
+              //                   ),
+              //                 ],
+              //               ),
+              //               ElevatedButton(onPressed: () {}, child: Text('Submit')),
+            ],
+          ),
+        ),
+      ),
     );
-    //         ),
-    //       ),
-    //     );
   }
 }
 
